5f75b4d01b2b18712a3c528425f6fc85d6cf6a00
Jill Wang <jill.gisrd@gmail.com>
2017-03-08 01:06:35 +0800

    加入階段的說明

--- amiq/js/App.jsx
+++ amiq/js/App.jsx
@@ -9,10 +9,11 @@ class App extends React.Component {
     this.state = {
       data: []
     }
+    this.renderBooksView = this.renderBooksView.bind(this);
   }
 
   componentDidMount() {
-    fetch('data/data2.json')
+    fetch('data/data.json')
     .then((response) => {
       return response.json();
     }).then((res) => {
@@ -30,26 +31,45 @@ class App extends React.Component {
     })
   }
 
+  renderBooksView(data){
+    return (
+      data.map(book => (
+        <div className={`item stage stage-${book.stage}`} key={book.id}>
+          <div className="info">
+            <span className="title">
+              <span className="id">{book.id}</span> 
+              {book.title}
+              <span className="en">{book['title-en']}</span>
+            </span>        
+            <span className="subject">
+              {book.subject}
+            </span>
+          </div>
+          <div ref="preview" className="topics">
+            <img src={`/img/content/amiq${book.id}.jpg`} onError="this.src='/img/content/none.jpg'" />
+          </div>
+        </div>
+      ))
+    )
+  }
+
+  toggleBooksHandler(id){
+    $('.books' + id).toggle();
+  }
+
   render() {
     return (
       <div className="grid">
-        {this.state.showLoading ? <Loading /> : ''} 
-        {this.state.data.map(item => (
-          <div className={`item stage stage-${item.stage}`} key={item.id}>
-            <div className="info">
-              <span className="title">
-                <span className="id">{item.id}</span> 
-                {item.title}
-                <span className="en">{item['title-en']}</span>
-              </span>        
-              <span className="subject">
-                {item.subject}
-              </span>
+        {this.state.data.map(stage => (
+          <section key={stage.stage}>
+            <a className={`stage-bg stage-bg-${stage.stage}`} onClick={this.toggleBooksHandler(stage.stage)}>
+              <h2>第 {stage.stage} 階</h2>
+              <p>{stage.content}</p>
+            </a>
+            <div ref={`books-${stage.stage}`} className={`books books-${stage.stage}`}>
+              {this.renderBooksView(stage.books)}
             </div>
-            <div ref="preview" className="topics">
-              <img src={`/img/content/amiq${item.id}.jpg`} onError="this.src='/img/content/none.jpg'" />
-            </div>
-          </div>
+          </section>
         ))}
         <div ref="preview" className="preview">
           <img src="/img/content/106.jpg" />
