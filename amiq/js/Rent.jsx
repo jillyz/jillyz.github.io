d5a894897df14ee00cb0ea5853bad2a0ddd1de04
Jill Wang <jill.gisrd@gmail.com>
2017-03-23 10:13:40 +0800

    Fix: <Rent/> overflow: hidden/auto; ----> ( display <Terms/> or iFrame ) ;   fail to try iframe contents element ;

--- amiq/js/Rent.jsx
+++ amiq/js/Rent.jsx
@@ -14,7 +14,18 @@ class Rent extends React.Component {
   }
 
   componentDidMount() {
-    this.setState({showLoading: false})
+    this.setState({showLoading: false});
+
+    $("#rentForm").contents().find(".quantumWizButtonPaperbuttonEl").click(()=>{
+      alert('send');
+    });
+
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const isAgree = this.state.agree;
+    if(isAgree) { $('body').css({'overflow':'hidden'})};
+    if(!isAgree) { $('body').css({'overflow':'auto'})};    
   }
 
 
@@ -49,7 +60,7 @@ class Rent extends React.Component {
       <div className="app-wrap">
         {this.state.agree == false ? <Terms/> : '' }
         {this.state.agree == false ? this.agreeView() : '' }
-        {this.state.agree ? <iframe className="rent-form" src="https://docs.google.com/forms/d/e/1FAIpQLSdypAzaM8glHVhTUP9I4wNG1M-E9aUAujoAsB5qwiuAMCDEcQ/viewform"></iframe> : ''}        
+        {this.state.agree ? <iframe id="rentForm" className="rent-form" src="https://docs.google.com/forms/d/e/1FAIpQLSdypAzaM8glHVhTUP9I4wNG1M-E9aUAujoAsB5qwiuAMCDEcQ/viewform"></iframe> : ''}        
         {this.state.showLoading ? <div className="loading-small_xx"><Loading /></div> : ''} 
       </div>
     )
