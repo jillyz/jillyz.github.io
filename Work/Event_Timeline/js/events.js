79b46a77f3b73febc33ed27d5ff9975401854ae7
jillyz <jill.gisrd@gmail.com>
2017-05-12 15:23:13 +0800

    [Update] format

--- Work/Event_Timeline/js/events.js
+++ Work/Event_Timeline/js/events.js
@@ -42,7 +42,7 @@
     	// } else {
     	// 	$(this).addClass("pos1");
     	// }
-		if ( index % 2 == 1 ) {
+	if ( index % 2 == 1 ) {
     		$(this).addClass("pos2");
     	} else {
     		$(this).addClass("pos1");
@@ -78,49 +78,48 @@
     //--------------------------------------------------------------------------------------//
     
     window['zoomRange']=[
-    					 space_range_start,
-    				     space_range_start*5,
-    				     space_range_start*30,
-    				     space_range_start*55,
-    				     space_range_start*100];
+	    space_range_start,
+	    space_range_start*5,
+	    space_range_start*30,
+	    space_range_start*55,
+	    space_range_start*100];
     window['defaultStep']=0;
 
-	// 時間軸 以年為間距 ------------------------------------
+    // 時間軸 以年為間距 ------------------------------------
 
     $("#zoomYear").click(function() {
     	var i = $(window).width() - 140;
     	$(".timeline [data-year]").animate({'width' : i }, 300 );
     	$(".timeline [data-date]").each(function(){
-	    	var date = $(this).attr("data-date");
-	    	var cssLeft = ( i /365 ) * date ;
-	    	$(this).animate({'left': cssLeft - 6 }, 500 );
-	    });
-	    $(".timeline").animate({'left': 0 } , 300 );
+	    var date = $(this).attr("data-date");
+	    var cssLeft = ( i /365 ) * date ;
+	    $(this).animate({'left': cssLeft - 6 }, 500 );
+	});
+	$(".timeline").animate({'left': 0 } , 300 );
 
-	    window['defaultStep']=0;
-	    $(this).attr('disabled','disabled');
-	    $("#zoomOut, #zoomAll,#zoomIn").removeAttr('disabled');
+	window['defaultStep']=0;
+	$(this).attr('disabled','disabled');
+	$("#zoomOut, #zoomAll,#zoomIn").removeAttr('disabled');
     });
 
     // 時間軸 看全部 ------------------------------------
 
     $("#zoomAll").click(function() {
-    	
     	$(".tip-no-event, .tip-go-all").hide();
 
     	var i = window['zoomRange'][0];
     	$(".timeline [data-year]").animate({'width' : i }, 300);
     	$(".timeline [data-date]").each(function(){
-	    	var date = $(this).attr("data-date");
-	    	var cssLeft = ( i /365 ) * date ;
-	    	$(this).animate({'left': cssLeft - 6 },300);
-	    });
-	    $(".timeline").animate({'left': '0' } , 300 );
+	    var date = $(this).attr("data-date");
+	    var cssLeft = ( i /365 ) * date ;
+	    $(this).animate({'left': cssLeft - 6 },300);
+	});
+	$(".timeline").animate({'left': '0' } , 300 );
 
-	    window['defaultStep']=0;
-	    $(this).attr('disabled','disabled');
-	    $("#zoomOut").attr('disabled','disabled');
-	    $("#zoomIn, #zoomYear").removeAttr('disabled');
+	window['defaultStep']=0;
+	$(this).attr('disabled','disabled');
+	$("#zoomOut").attr('disabled','disabled');
+	$("#zoomIn, #zoomYear").removeAttr('disabled');
     });
 
     $(".timeline").dblclick(function() {
@@ -136,20 +135,20 @@
     	window['defaultStep']++;
     	if(window['defaultStep']>=window['zoomRange'].length)
     	{
-    		window['defaultStep']=window['zoomRange'].length-1;
-    		$(this).attr('disabled','disabled');
+    	    window['defaultStep']=window['zoomRange'].length-1;
+    	    $(this).attr('disabled','disabled');
     	} 
 
     	$("#zoomAll,#zoomOut,#zoomYear").removeAttr('disabled');	
     	var i = window['zoomRange'][(window['defaultStep']%window['zoomRange'].length)];
     	$(".timeline [data-year]").animate({'width' : i}, 300);
-	    $(".timeline [data-date]").each(function(){
-	    	var date = $(this).attr("data-date");
-	    	var cssLeft = ( i /365 ) * date ;
-	    	$(this).animate({'left': cssLeft - 6 },300);
-	    }); 
+	$(".timeline [data-date]").each(function(){
+	    var date = $(this).attr("data-date");
+	    var cssLeft = ( i /365 ) * date ;
+	    $(this).animate({'left': cssLeft - 6 },300);
+	}); 
 
-	    $(".timeline").animate({'left': '0' } , 300 ); 	
+	$(".timeline").animate({'left': '0' } , 300 ); 	
     });
 
     // 時間軸 縮小 ------------------------------------
@@ -159,23 +158,23 @@
     	$(".tip-no-event, .tip-go-all").hide();
 
     	window['defaultStep']--;
-    	if(window['defaultStep']<0)
+    	if(window['defaultStep'] < 0)
     	{
-    		window['defaultStep']=0;
-    		$(this).attr('disabled','disabled');
-    		$("#zoomAll").attr('disabled','disabled');
+    	    window['defaultStep'] = 0;
+    	    $(this).attr('disabled','disabled');
+    	    $("#zoomAll").attr('disabled','disabled');
     	}
     	$("#zoomIn,#zoomYear").removeAttr('disabled');
     	var i = window['zoomRange'][(window['defaultStep']%window['zoomRange'].length)];
-    	i=(i<0)?0:i;
+    	i = (i < 0) ? 0 : i;
     	$(".timeline [data-year]").animate({'width' : i}, 300);
- 	    $(".timeline [data-date]").each(function(){
-	    	var date = $(this).attr("data-date");
-	    	var cssLeft = ( i/365 ) * date ;
-	    	$(this).animate({'left': cssLeft - 6 },300);
-	    }); 
+ 	$(".timeline [data-date]").each(function(){
+	    var date = $(this).attr("data-date");
+	    var cssLeft = ( i/365 ) * date ;
+	    $(this).animate({'left': cssLeft - 6 },300);
+	}); 
 
-	    $(".timeline").animate({'left': '0' } , 300 );    	
+	$(".timeline").animate({'left': '0' } , 300 );    	
     });
 
     // 時間軸 直接跳到某一年 ------------------------------------
@@ -187,28 +186,27 @@
     	var i = $(window).width() - 140;    	
     	$(".timeline [data-year]").animate({'width' : i }, 300 );
     	$(".timeline [data-date]").each(function(){
-	    	var date = $(this).attr("data-date");
-	    	var dateLeft = ( i /365 ) * date ;
-	    	$(this).animate({'left': dateLeft - 6 }, 500 );
-	    });
+	    var date = $(this).attr("data-date");
+	    var dateLeft = ( i /365 ) * date ;
+	    $(this).animate({'left': dateLeft - 6 }, 500 );
+	});
 
-	    var n = $(this).parent().index() ;
-	    $(".timeline").animate({'left': "-" + ((i*n)+10) + "px" } , 300 );
-	    
-	    window['defaultStep']=0;
-	    $("#zoomYear").attr('disabled','disabled');
-	    $("#zoomOut, #zoomAll,#zoomIn").removeAttr('disabled');
+	var n = $(this).parent().index() ;
+	$(".timeline").animate({'left': "-" + ((i*n)+10) + "px" } , 300 );
 
-	    var child = $(this).parent().children("a").size();
-	    if ( child == 0 ) {
-	    	$(".tip-no-event").show();
-	    }
-	    $(".tip-go-all").show();
+	window['defaultStep']=0;
+	$("#zoomYear").attr('disabled','disabled');
+	$("#zoomOut, #zoomAll,#zoomIn").removeAttr('disabled');
+
+	var child = $(this).parent().children("a").size();
+	if ( child == 0 ) {
+	    $(".tip-no-event").show();
+	}
+	$(".tip-go-all").show();
     });
 
     //初始化排序類型與方向
-    $(".main .order a").removeClass("descending");
-    $(".main .order a").removeClass("ascending");
+    $(".main .order a").removeClass("descending").removeClass("ascending");
     $(".main .order a[req='sdatetime']").addClass("descending");
               
   });
