c9178ea314507a1d43e1a6bbd5dc20cbd05d58f9
Jill Wang <jill.wang@titansoft.com.sg>
2019-01-19 00:39:24 +0800

    game card=>option

--- amiq/js/Game.jsx
+++ amiq/js/Game.jsx
@@ -8,8 +8,16 @@ class Game extends React.Component {
         super(props);
         this.state = {
             topics: [
-                '106_1',
-                '106_6'
+                {
+                    'id': '106_1',
+                    'opening': '早安！起床準備上學囉～',
+                    'question': '找出符合的睡衣和襪子唷',
+                },
+                {
+                    'id': '106_6',
+                    'opening': '彼得畫了這張美麗的圖',
+                    'question': '他用了哪些顏色呢？請指出來',
+                },
             ],
             currentTopic: '',
         }
@@ -35,6 +43,29 @@ class Game extends React.Component {
                 }
             }
         }
+
+        var currentCard = 0;
+
+        $('.game-card a').not('.active').click(function(){
+            currentCard = parseInt($(this).attr('data-card'));
+            $(this).addClass('active');
+            console.log('currentCard',currentCard);
+        });
+
+        $('.game-option a').click(function(){
+            if(currentCard > 0) {
+                $(this).attr('data-showoption',currentCard );
+                currentCard = 0;
+            }
+        });
+        
+        $('.game-card a.active').click(function(){
+            $(this).removeClass('active');
+            var removeCard = parseInt($(this).attr('data-card'));
+            console.log('removeCard',removeCard);
+            $('.game-option a[data-showoption="' + removeCard + '"]').attr('data-showoption' , 0);
+        });
+
     }
     componentDidUpdate(prevProps, prevState) {
     }
@@ -52,12 +83,22 @@ class Game extends React.Component {
 
         )
     }
-    renderLiList() {
+    renderCardList() {
         let Li = []
 
         for (let i = 0; i < 12; i++) {
             Li.push(
-                <li>{i + 1}</li>
+                <li><a data-card={i + 1}>{i + 1}</a></li>
+            );
+        }
+        return Li;
+    }
+    renderOptionList() {
+        let Li = []
+
+        for (let i = 0; i < 12; i++) {
+            Li.push(
+                <li><a></a></li>
             );
         }
         return Li;
@@ -65,19 +106,32 @@ class Game extends React.Component {
 
     render() {
         const topic = this.state.currentTopic;
+        const style = {
+            backgroundImage: `url(./img/game/topic/${topic.id}.jpg`,
+        };
         return (
             <div className="game-wrap">
-                <div>
-                    <img className="game-topic-img" src={`./img/game/topic/${topic}.jpg`} alt="題目" />
-                    <div className="game-option">
+                <div className="game-question-sample">
+                    <div className="game-question-text">
+                        <p>{topic.opening}</p>
+                        <p>{topic.question}</p>
+                    </div>
+                    <div className="game-answer-sample" style={style}></div>
+                </div>
+                <div className="game-question-img" style={style}></div>
+                <div className="game-option-img" style={style}>
+                    <div className="game-option game-list">
                         <ul>
-                            {this.renderLiList()}
+                            {this.renderOptionList()}
                         </ul>
                     </div>
                 </div>
-                <div className="game-card">
+                <div>
+                    {/* <img width="100%" className="game-topic-img" src={`./img/game/topic/${topic.id}.jpg`} alt="題目" /> */}
+                </div>
+                <div className="game-card game-list">
                     <ul>
-                        {this.renderLiList()}
+                        {this.renderCardList()}
                     </ul>
                 </div>
             </div>
