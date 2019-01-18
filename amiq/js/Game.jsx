f9e78436061b33adf2da8d33228fc9a4c206f9a9
Jill Wang <jill.wang@titansoft.com.sg>
2019-01-18 12:15:06 +0800

    Game try Full Screen

--- amiq/js/Game.jsx
+++ amiq/js/Game.jsx
@@ -17,6 +17,24 @@ class Game extends React.Component {
 
     componentDidMount() {
         this.getRandomTopic();
+
+        window.onload = maxWindow;
+
+        function maxWindow() {
+            window.moveTo(0, 0);
+
+
+            if (document.all) {
+                top.window.resizeTo(screen.availWidth, screen.availHeight);
+            }
+
+            else if (document.layers || document.getElementById) {
+                if (top.window.outerHeight < screen.availHeight || top.window.outerWidth < screen.availWidth) {
+                    top.window.outerHeight = screen.availHeight;
+                    top.window.outerWidth = screen.availWidth;
+                }
+            }
+        }
     }
     componentDidUpdate(prevProps, prevState) {
     }
