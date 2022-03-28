1dec64f8c6fadb2455ef2ac023e7969095873d55
jillyz <jill.gisrd@gmail.com>
2022-03-28 14:27:21 +0800

    經費頁

--- WraDashboard/js/dashboard-plan-priority.js
+++ WraDashboard/js/dashboard-plan-priority.js
@@ -1,12 +1,12 @@
 requirejs([
     'common',
     'echart/echart-funds-1',
-    // 'echart/echart-funds-2-1',
-    // 'echart/echart-funds-2-2',
+    'echart/echart-funds-2-1',
+    'echart/echart-funds-2-2',
     'echart/echart-funds-3-1',
     'echart/echart-funds-3-2',
 ], function() {
     renderChartFunds1();
-    // renderChartFunds21();
+    renderChartFunds21();
     renderChartFunds31();
 });
