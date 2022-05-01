7446bd7013ab5d84b8c0d222bf54df3b95c691a1
Jill Wang <>
2022-05-01 08:59:43 +0800

    fix JS : 修正主選單 - 下拉次選單問題

--- TaichungCityCOVID19/assets/js/base-layout-one.js
+++ TaichungCityCOVID19/assets/js/base-layout-one.js
@@ -1,65 +1,61 @@
-$(function(){
+$(function() {
 
     // 選單collapse event
-    $('#GSMis-collapse-menu').on('hide.bs.collapse', function () {
+    $('#GSMis-collapse-menu').on('hide.bs.collapse', function() {
         // 漢堡icon切換
         $(".GSMis-hamburger-mobile > *").toggle();
-        $('.GSMis-help-user-name').fadeOut();        
+        $('.GSMis-help-user-name').fadeOut();
     })
-    $('#GSMis-collapse-menu').on('show.bs.collapse', function () {
+    $('#GSMis-collapse-menu').on('show.bs.collapse', function() {
         // 漢堡icon切換
         $(".GSMis-hamburger-mobile > *").toggle();
         $('.GSMis-help-user-name').fadeIn();
     })
 
-    
-   // 所有祖輩給.active
-   $('.GSMis-menu .current').parents('li').addClass('active');
-   // 手機模式時，所有.active的子輩.children給.childrenOpen
-    if($(window).width() < 992){
+
+    // 所有祖輩給.active
+    $('.GSMis-menu .current').parents('li').addClass('active');
+    // 手機模式時，所有.active的子輩.children給.childrenOpen
+    if ($(window).width() < 992) {
         $('.GSMis-menu .active > .children').addClass('childrenOpen');
-    }   
+    }
 
 
     // 子選單開闔    
     /*
         螢幕寬度<992都是click動作，以上是hover動作
     */
-    if($(window).width() < 992){
+    if ($(window).width() < 992) {
         // Click動作 (手機)
-        $(".GSMis-menu .children").click(function(e){
+        $(".GSMis-menu .children").click(function(e) {
             e.preventDefault();
             $(this).toggleClass('childrenOpen');
             $(this).next().slideToggle();
-        })        
-    }else if($(window).width() >= 992 && $(window).width() <= 1024){
+        })
+    } else if ($(window).width() >= 992 && $(window).width() <= 1024) {
         // Click動作 (iPad橫式)
-        $(".GSMis-menu > li > .children").click(function(e){
+        $(".GSMis-menu > li > .children").click(function(e) {
             e.preventDefault();
             $(this).toggleClass('childrenOpen');
             $(this).next().slideToggle();
 
             $(this).parent().siblings().children('.level-2').slideUp();
         })
-        $(".GSMis-menu .level-2 .children").click(function(e){
+        $(".GSMis-menu .level-2 .children").click(function(e) {
             e.preventDefault();
             $(this).toggleClass('childrenOpen');
             $(this).next().slideToggle();
         })
-    }else {
+    } else {
         // Hover動作 (>992)
-        $(".GSMis-menu > li").hover(function(){
-            $(this).children(".children").toggleClass('childrenOpen').next().finish().slideToggle();
+        $(".GSMis-menu > li").hover(function() {
+            $(this).children(".children").toggleClass('childrenOpen').next().stop().slideToggle();
         })
-        $(".GSMis-menu .level-2 .children").click(function(e){
+        $(".GSMis-menu .level-2 .children").click(function(e) {
             e.preventDefault();
             $(this).toggleClass('childrenOpen');
             $(this).next().slideToggle();
         })
-    }   
-    
+    }
+
 })
-
-
-
-
