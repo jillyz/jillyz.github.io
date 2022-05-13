d2da74e8a7e1e2a32e127b12d19846e34f504862
GIS\jillwang <jill.gisrd@gmail.com>
2022-05-13 09:52:15 +0800

    刪除功能

--- TaichungCityCOVID19/assets/js/common.js
+++ TaichungCityCOVID19/assets/js/common.js
@@ -4,10 +4,12 @@ $(() => {
     
     // go to top
     goTop();
+
+    // for DELETE
+    checkForAllDelete();
+    checkForDelete();
+
 });
-
-
-
 
 // == 自訂function =====================================
 // Go to top
@@ -20,3 +22,55 @@ const goTop = () => {
         }
     })
 };
+
+// check all checkbox for DELETE
+const checkForAllDelete = () => {
+    $('#chk_delete_all').change(function(){
+        const isChecked = this.checked;
+        if(isChecked){
+            $('.chk-del').prop( "checked", true );
+            $('.chk-del').parent().parent('tr').addClass('delete-highlight');
+        }
+        else{
+            $('.chk-del').prop( "checked", false );
+            $('.chk-del').parent().parent('tr').removeClass('delete-highlight');
+        }
+        showDeleteFunc();
+    })
+}
+
+// check single checkbox for DELETE
+const checkForDelete = () => {
+    $('.chk-del').change(function(){
+        // highlight tr
+        const isChecked = this.checked;
+        if(isChecked){
+            $(this).parent().parent('tr').addClass('delete-highlight');
+        }
+        else{
+            $(this).parent().parent('tr').removeClass('delete-highlight');
+        }
+
+        const countInputAll = $('.chk-del').length;
+        const countInputIsChecked = $('.chk-del:checked').length;
+        if(countInputIsChecked == countInputAll) {
+            $('#chk_delete_all').prop( "checked", true );
+        }
+        else{
+            $('#chk_delete_all').prop( "checked", false );
+        }
+
+        showDeleteFunc();
+    })
+};
+
+// show DELETE fun panel
+const showDeleteFunc = () => {
+    const countChecked =$('td.delete input[type=checkbox]:checked').length;
+    if(countChecked > 0) {
+        $('.delete-wrap').addClass('show');
+    }
+    else{
+        $('.delete-wrap').removeClass('show');
+    }
+}
